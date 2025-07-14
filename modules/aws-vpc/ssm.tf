1d09c1f205d27200d30c0177eedb1c7ce49a1a33
ulises-jeremias <ulisescf.24@gmail.com>
2025-07-13 22:26:42 -0300

    fix: Improve handling of optional SSM parameter values in VPC module

--- modules/aws-vpc/ssm.tf
+++ modules/aws-vpc/ssm.tf
@@ -24,31 +24,31 @@ resource "aws_ssm_parameter" "vpc_cidr_block" {
 }
 
 resource "aws_ssm_parameter" "database_subnets" {
-  count = var.create_ssm_parameters && length(module.vpc.database_subnets) > 0 ? 1 : 0
+  count = var.create_ssm_parameters ? 1 : 0
 
   name  = "${local.ssm_prefix}/database_subnets"
   type  = "StringList"
-  value = join(",", module.vpc.database_subnets)
+  value = join(",", try(module.vpc.database_subnets, []))
 
   tags = var.tags
 }
 
 resource "aws_ssm_parameter" "database_subnet_group" {
-  count = var.create_ssm_parameters && module.vpc.database_subnet_group != null ? 1 : 0
+  count = var.create_ssm_parameters ? 1 : 0
 
   name  = "${local.ssm_prefix}/database_subnet_group"
   type  = "String"
-  value = module.vpc.database_subnet_group
+  value = try(module.vpc.database_subnet_group, "")
 
   tags = var.tags
 }
 
 resource "aws_ssm_parameter" "database_subnet_group_name" {
-  count = var.create_ssm_parameters && module.vpc.database_subnet_group_name != null ? 1 : 0
+  count = var.create_ssm_parameters ? 1 : 0
 
   name  = "${local.ssm_prefix}/database_subnet_group_name"
   type  = "String"
-  value = module.vpc.database_subnet_group_name
+  value = try(module.vpc.database_subnet_group_name, "")
 
   tags = var.tags
 }
@@ -104,21 +104,21 @@ resource "aws_ssm_parameter" "availability_zones" {
 }
 
 resource "aws_ssm_parameter" "internet_gateway_id" {
-  count = var.create_ssm_parameters && module.vpc.igw_id != null ? 1 : 0
+  count = var.create_ssm_parameters ? 1 : 0
 
   name  = "${local.ssm_prefix}/internet_gateway_id"
   type  = "String"
-  value = module.vpc.igw_id
+  value = try(module.vpc.igw_id, "")
 
   tags = var.tags
 }
 
 resource "aws_ssm_parameter" "nat_gateway_ids" {
-  count = var.create_ssm_parameters && length(module.vpc.natgw_ids) > 0 ? 1 : 0
+  count = var.create_ssm_parameters ? 1 : 0
 
   name  = "${local.ssm_prefix}/nat_gateway_ids"
   type  = "StringList"
-  value = join(",", module.vpc.natgw_ids)
+  value = join(",", try(module.vpc.natgw_ids, []))
 
   tags = var.tags
 }
