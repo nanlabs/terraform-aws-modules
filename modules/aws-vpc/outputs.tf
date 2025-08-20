eeb9f21e5cdf9778819974acfe7a0abd738571c5
Ulises Jeremias <ulisescf.24@gmail.com>
2025-08-20 14:51:48 -0300

    Update outputs.tf

--- modules/aws-vpc/outputs.tf
+++ modules/aws-vpc/outputs.tf
@@ -20,52 +20,52 @@ output "default_security_group_id" {
   value       = module.vpc.default_security_group_id
 }
 
-# SSM Parameters
+# SSM Parameters (referenciando locals y variables en vez de recursos)
 output "ssm_parameter_vpc_id" {
   description = "name of the ssm parameter for the vpc id"
-  value       = var.create_ssm_parameters ? aws_ssm_parameter.vpc_id[0].name : null
+  value       = var.create_ssm_parameters ? "${local.ssm_prefix}/vpc_id" : null
 }
 
 output "ssm_parameter_public_subnets" {
   description = "name of the ssm parameter for the public subnets"
-  value       = var.create_ssm_parameters ? aws_ssm_parameter.public_subnets[0].name : null
+  value       = local.create_public_subnets_ssm ? "${local.ssm_prefix}/public_subnets" : null
 }
 
 output "ssm_parameter_private_subnets" {
   description = "name of the ssm parameter for the private subnets"
-  value       = var.create_ssm_parameters ? aws_ssm_parameter.private_subnets[0].name : null
+  value       = local.create_private_subnets_ssm ? "${local.ssm_prefix}/private_subnets" : null
 }
 
 output "ssm_parameter_database_subnets" {
   description = "name of the ssm parameter for the database subnets"
-  value       = var.create_ssm_parameters && length(module.vpc.database_subnets) > 0 ? aws_ssm_parameter.database_subnets[0].name : null
+  value       = local.create_database_subnets_ssm ? "${local.ssm_prefix}/database_subnets" : null
 }
 
 output "ssm_parameter_app_subnets" {
   description = "name of the ssm parameter for the app subnets"
-  value       = var.create_ssm_parameters ? aws_ssm_parameter.app_subnets[0].name : null
+  value       = var.create_ssm_parameters ? "${local.ssm_prefix}/app_subnets" : null
 }
 
 output "ssm_parameter_app_security_group" {
   description = "name of the ssm parameter for the app security group"
-  value       = var.create_ssm_parameters ? aws_ssm_parameter.app_security_group[0].name : null
+  value       = var.create_ssm_parameters ? "${local.ssm_prefix}/app_security_group" : null
 }
 
-# SSM Parameter Names Output
+# SSM Parameter Names Output (usando locals y variables)
 output "ssm_parameter_names" {
   description = "Names of the created SSM parameters for VPC details"
   value = var.create_ssm_parameters ? {
-    vpc_id                     = "${var.ssm_parameter_prefix != "" ? var.ssm_parameter_prefix : "/${var.name}"}/vpc_id"
-    vpc_cidr_block             = "${var.ssm_parameter_prefix != "" ? var.ssm_parameter_prefix : "/${var.name}"}/vpc_cidr_block"
-    database_subnets           = length(module.vpc.database_subnets) > 0 ? "${var.ssm_parameter_prefix != "" ? var.ssm_parameter_prefix : "/${var.name}"}/database_subnets" : null
-    database_subnet_group      = module.vpc.database_subnet_group != null ? "${var.ssm_parameter_prefix != "" ? var.ssm_parameter_prefix : "/${var.name}"}/database_subnet_group" : null
-    database_subnet_group_name = module.vpc.database_subnet_group_name != null ? "${var.ssm_parameter_prefix != "" ? var.ssm_parameter_prefix : "/${var.name}"}/database_subnet_group_name" : null
-    public_subnets             = "${var.ssm_parameter_prefix != "" ? var.ssm_parameter_prefix : "/${var.name}"}/public_subnets"
-    private_subnets            = "${var.ssm_parameter_prefix != "" ? var.ssm_parameter_prefix : "/${var.name}"}/private_subnets"
-    app_subnets                = "${var.ssm_parameter_prefix != "" ? var.ssm_parameter_prefix : "/${var.name}"}/app_subnets"
-    app_security_group         = "${var.ssm_parameter_prefix != "" ? var.ssm_parameter_prefix : "/${var.name}"}/app_security_group"
-    availability_zones         = "${var.ssm_parameter_prefix != "" ? var.ssm_parameter_prefix : "/${var.name}"}/availability_zones"
-    internet_gateway_id        = module.vpc.igw_id != null ? "${var.ssm_parameter_prefix != "" ? var.ssm_parameter_prefix : "/${var.name}"}/internet_gateway_id" : null
-    nat_gateway_ids            = length(module.vpc.natgw_ids) > 0 ? "${var.ssm_parameter_prefix != "" ? var.ssm_parameter_prefix : "/${var.name}"}/nat_gateway_ids" : null
+    vpc_id                     = "${local.ssm_prefix}/vpc_id"
+    vpc_cidr_block             = "${local.ssm_prefix}/vpc_cidr_block"
+    database_subnets           = local.create_database_subnets_ssm ? "${local.ssm_prefix}/database_subnets" : null
+    database_subnet_group      = local.create_database_subnet_group_ssm ? "${local.ssm_prefix}/database_subnet_group" : null
+    database_subnet_group_name = local.create_database_subnet_group_name_ssm ? "${local.ssm_prefix}/database_subnet_group_name" : null
+    public_subnets             = local.create_public_subnets_ssm ? "${local.ssm_prefix}/public_subnets" : null
+    private_subnets            = local.create_private_subnets_ssm ? "${local.ssm_prefix}/private_subnets" : null
+    app_subnets                = "${local.ssm_prefix}/app_subnets"
+    app_security_group         = "${local.ssm_prefix}/app_security_group"
+    availability_zones         = "${local.ssm_prefix}/availability_zones"
+    internet_gateway_id        = local.create_internet_gateway_ssm ? "${local.ssm_prefix}/internet_gateway_id" : null
+    nat_gateway_ids            = local.create_nat_gateway_ssm ? "${local.ssm_prefix}/nat_gateway_ids" : null
   } : {}
 }
